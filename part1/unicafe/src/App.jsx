77c5c95b213fff18b14505c8da68e0e30be9f9d4
AlejandroSuero <asuerome@gmail.com>
2023-09-22 21:37:54 +0200

    step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,8 +16,25 @@ const BadButton = ({ text, handleClick }) => (
   </button>
 )
 const Statistic = ({statistic, value}) => (
-  <p>{statistic} {value}</p>
+  <p>{statistic}: {value}</p>
 )
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = all !== 0 ? (good * 1 + bad * -1) / all : 0
+  const positive = all !== 0 ? good * (100 / all) : 0
+
+  return (
+    <div>
+      <Statistic statistic={"good"} value={good} />
+      <Statistic statistic={"neutral"} value={neutral} />
+      <Statistic statistic={"bad"} value={bad} />
+      <Statistic statistic={"all"} value={all} />
+      <Statistic statistic={"average"} value={average} />
+      <Statistic statistic={"positive"} value={`${positive} %`} />
+    </div>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -31,9 +48,7 @@ const App = () => {
       <NeutralButton text={"neutral"} handleClick={() => setNeutral(neutral + 1)} />
       <BadButton text={"bad"} handleClick={() => setBad(bad + 1)} />
       <h1>statistics</h1>
-      <Statistic statistic={"good"} value={good} />
-      <Statistic statistic={"neutral"} value={neutral} />
-      <Statistic statistic={"bad"} value={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
